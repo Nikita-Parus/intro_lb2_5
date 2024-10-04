2754a899693dd61152fc8290de2a0eafcb33fa86
Nikita-Parus <cheems.nz@gmail.com>
2024-10-04 19:43:27 +0300

    Добавьте файлы проекта.

---
+++ intro_lb2_5/intro_lb2_5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	printf("a: %p, b: %p",&a, &b);
+	for (int i = a; i <= b; i++) {
+		cout << i * -1 << endl;
+	}
+}
